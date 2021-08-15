b99634c2e8fec6a9d460f07ded6ed732cf4694b1
danisbagus <danisbagussetiawan@gmail.com>
2021-08-15 11:40:16 +0700

    Create users table

--- resource/docker/initdb/01-semimarket.sql
+++ resource/docker/initdb/01-semimarket.sql
@@ -20,6 +20,26 @@ INSERT INTO `products` VALUES
 
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+
+INSERT INTO `users` VALUES 
+  (1,'admin','abc123','admin','2020-08-09 10:27:22'),
+  (2,'2001','abc123','user','2020-08-09 10:27:22'),
+  (3,'2000','abc123','user','2020-08-09 10:27:22');
+
+UNLOCK TABLES;
+
 DROP TABLE IF EXISTS `transactions`;
 
 CREATE TABLE `transactions` (
